37e8add26eee1500c60110cbd3fe2de6388ee419
Jardel Osorio Duarte <jardelduarte594@gmail.com>
2019-11-10 02:33:53 -0300

    incluindo um modelo exemplo

--- index.js
+++ index.js
@@ -10,32 +10,14 @@ app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
-app.use('/', (req, res) =>  {
+app.get('/', (req, res) =>  {
   res.render('index.html')
+})
+
+app.get('/chat', (req, res) => {
+	res.render('routes/home.html')
 })
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na url http://localhost:"+PORT)  
 })
-
-
-/*
-const express = require('express') 
-const app = express()
-const path = require('path')
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('views engine', 'html')
-
-app.use('/', (req, res) => {
-    res.render('index.html')
-})
-
-app.listen(PORT, () => {
-  console.log('server in http://localhost'+PORT)
-})
-*/
